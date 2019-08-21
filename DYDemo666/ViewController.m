75b3ee3b4bde62fe953dc864d928b6ff7057e461
Lambert <447674668@qq.com>
2019-08-21 15:21:14 +0800

    更新pod

--- DYDemo666/ViewController.m
+++ DYDemo666/ViewController.m
@@ -25,9 +25,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     __weak typeof(self) weakSelf = self;
-    self.navigationItem.rightBarButtonItem = [[KHBarButton rightBtnWithTitle:@"哈哈哈" Color:[UIColor blackColor] ClickOption:^{
-        [weakSelf addImageDemo];
+    self.navigationItem.rightBarButtonItem = [[KHBarButton rightBtnWithTitle:@"个人信息" Color:[UIColor blackColor] ClickOption:^{
+        [weakSelf.navigationController pushViewController:[[CTMediator sharedInstance] ingoVC] animated:YES];
+    }] getBarItem];
+    
+    self.navigationItem.leftBarButtonItem = [[KHBarButton leftBtnWithTitle:@"关于我们" Color:[UIColor blackColor] ClickOption:^{
+        NSString *urlStr = @"dydemo666://DYDemoLogin/AboutVC?name=heiheihei";
+        UIViewController *vc = [[CTMediator sharedInstance] performActionWithUrl:[NSURL URLWithString:urlStr] completion:nil];
+        [weakSelf.navigationController pushViewController:vc animated:YES];
     }] getBarItem];
     
     UIButton *loginBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
@@ -37,9 +44,7 @@
     [loginBtn setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
     [loginBtn addTarget:self action:@selector(loginBtnClick:) forControlEvents:(UIControlEventTouchUpInside)];
     [self.view addSubview:loginBtn];
-}
-
-- (void)addImageDemo {
+    
     UIImage *img = [DYDemoTools getToolsBundleImage:@"img1"];
     UIImageView *iV = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, img.size.width, img.size.height)];
     iV.image = img;
